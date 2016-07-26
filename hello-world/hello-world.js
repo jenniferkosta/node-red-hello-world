a6009763812f4fe1d5c91214ef755a2702708072
jenniferkosta <jennifer.kosta@web.de>
2016-07-26 09:57:42 +0200

    Create hello-world.js

---
+++ hello-world/hello-world.js
@@ -0,0 +1,12 @@
+module.exports = function (RED) {
+	"use strict";
+	function HelloWorldNode(config){
+		RED.nodes.createNode(this, config);
+		var node = this;
+		this.on('input', function (msg){
+			msg.payload = "Hello World";
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType("hello-world", HelloWorldNode);
+};
